98384e729ddfbe53d092db8f3200ecc025e1566a
Joseph <jwilde19@gmail.com>
2022-02-15 18:09:57 -0500

    Combine multiple API's requests to show more movies

--- src/App.js
+++ src/App.js
@@ -7,25 +7,54 @@ import './App.css';
 function App() {
 
   const [trending, setTrending] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [unique, setUnique] = useState([]);
   const [filtered, setFiltered] = useState([]);
   const [activeGenre, setActiveGenre] = useState(0);
 
   useEffect(() => {
-    fetchTrending();
+    fetchAllMovies();
   },[]);
 
-  const fetchTrending = async () => {
-    const trendingData = await fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=f90b1cd271cd92b5e20663e52ddf51a4')
+  const fetchAllMovies = async () => {
+    // Fetch trending movies from themoviedb.org
+    const trendingData = await fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=f90b1cd271cd92b5e20663e52ddf51a4');
     const trendingMovies = await trendingData.json();
     setTrending(trendingMovies.results);
-    setFiltered(trendingMovies.results);
+    // Fetch popular movies from themoviedb.org
+    const popularData = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=f90b1cd271cd92b5e20663e52ddf51a4&language=en-US&page=1');
+    const popularMovies = await popularData.json();
+    setPopular(popularMovies.results);
+    // Fetch top rated movies from themoviedb.org
+    const topRatedData = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=f90b1cd271cd92b5e20663e52ddf51a4&language=en-US&page=1');
+    const topRatedMovies = await topRatedData.json();
+    setTopRated(topRatedMovies.results);
+    // Combine all API data into one array
+    const filtered = trendingMovies.results.concat(popularMovies.results, topRatedMovies.results);
+    // Create new Set object to filter API data duplicates
+    const filteredMovies = new Set();
+    // Pull only movies with unique id's
+    const uniqueMovies = filtered.filter(movie => {
+      const duplicate = filteredMovies.has(movie.id);
+      filteredMovies.add(movie.id);
+      return !duplicate;
+    });
+    // Set only unique movies to filtered state
+    setFiltered(uniqueMovies);
+    setUnique(uniqueMovies);
+    console.log(uniqueMovies);
   }
+
 
   return (
     <div className="App">
-      <h1>What To Watch</h1>
+      <h1>Top Movies</h1>
       <Filters 
-        trending={trending} 
+        trending={trending}
+        popular={popular}
+        topRated={topRated}
+        unique={unique} 
         setFiltered={setFiltered}
         activeGenre={activeGenre}
         setActiveGenre={setActiveGenre} 
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,12 @@
 import {useEffect} from 'react';
 
-const Filters = ({ setActiveGenre, activeGenre, setFiltered, trending }) => {
+const Filters = ({ setActiveGenre, activeGenre, setFiltered, unique }) => {
     useEffect(() => {
         if(activeGenre === 0) {
-            setFiltered(trending);
+            setFiltered(unique);
             return;
         }
-        const filtered = trending.filter((movie) => 
+        const filtered = unique.filter((movie) => 
             movie.genre_ids.includes(activeGenre)
         );
         setFiltered(filtered);
@@ -16,7 +16,18 @@ const Filters = ({ setActiveGenre, activeGenre, setFiltered, trending }) => {
         <div className="filters">
             <button onClick={() => setActiveGenre(0)}>All</button>
             <button onClick={() => setActiveGenre(28)}>Action</button>
+            <button onClick={() => setActiveGenre(12)}>Adventure</button>
             <button onClick={() => setActiveGenre(35)}>Comedy</button>
+            <button onClick={() => setActiveGenre(80)}>Crime</button>
+            <button onClick={() => setActiveGenre(99)}>Documentary</button>
+            <button onClick={() => setActiveGenre(18)}>Drama</button>
+            <button onClick={() => setActiveGenre(10751)}>Family</button>
+            <button onClick={() => setActiveGenre(14)}>Fantasy</button>
+            <button onClick={() => setActiveGenre(27)}>Horror</button>
+            <button onClick={() => setActiveGenre(9648)}>Mystery</button>
+            <button onClick={() => setActiveGenre(10749)}>Romance</button>
+            <button onClick={() => setActiveGenre(878)}>Science Fiction</button>
+            <button onClick={() => setActiveGenre(53)}>Thriller</button>
         </div>
     )
 }
